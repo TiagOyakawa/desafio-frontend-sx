75ec74c4807c53b27134b32d18b608b1f7f8f45b
Tiago Pirico Oyakawa <tiagoyakawa@hotmail.com>
2024-08-14 14:49:12 -0300

    feat(home): inclusao de estilizacao

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
+import sxLogo from './../../assets/LOGO SX.png'
 import './style.css'
 
 function Home() {
 
     return (
-        <div>
-            <h1>Bem vindo !</h1>
-            <p>Junte-se a milhões de jogadores e faça novos amigos! Explore um mundo de possibilidades e torne-se um mestre dos games. A diversão está apenas um clique de distância.</p>
+        <div className='container-home'>
+            <img src={sxLogo} alt="Logo Sx" className='image' />
+            <h1 className='title'>Bem vindo !</h1>
+            <p className='text'>Junte-se a milhões de jogadores e faça novos amigos! Explore um mundo de possibilidades e torne-se um mestre dos games. A diversão está apenas um clique de distância.</p>
             <button>Jogue agora</button>
         </div>
     )
