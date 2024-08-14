5195105e613838d433aa159721c43d7eac50110d
Tiago Pirico Oyakawa <tiagoyakawa@hotmail.com>
2024-08-14 14:01:54 -0300

    feat(home): desenvolvimento da tela inicial

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import Home from './pages/Home'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+createRoot(document.getElementById('root')).render( 
+    <StrictMode>
+    <Home/>
+    </StrictMode>,
 )
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+function Home() {
+
+    return (
+        <div>
+            <h1>Bem vindo !</h1>
+            <p>Junte-se a milhões de jogadores e faça novos amigos! Explore um mundo de possibilidades e torne-se um mestre dos games. A diversão está apenas um clique de distância.</p>
+            <button>Jogue agora</button>
+        </div>
+    )
+}
+
+export default Home
