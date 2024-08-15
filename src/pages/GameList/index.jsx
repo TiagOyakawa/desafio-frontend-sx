5b0f142a7a10a62d65729d8560cdc6a8c1d7c17c
Tiago Pirico Oyakawa <tiagoyakawa@hotmail.com>
2024-08-14 23:34:28 -0300

    feat(game-list): criacao de pagina da listagem de jogos

--- src/pages/GameList/index.jsx
+++ src/pages/GameList/index.jsx
@@ -1,10 +1,35 @@
 import './style.css'
+import SxLogo from './../../assets/LOGO SX.png'
+import MockGameList from '../../mock/gameList.json'
 
 function GameList() {
+    const games = MockGameList
 
     return (
         <div className='container-game-list'>
-           GAMELIST
+            <header>
+                <img src={SxLogo} alt="Logo Sx" className='header-image' />
+                <ul className='menu'>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/games">Games</a>
+                    </li>
+                </ul>
+            </header>
+            <div class="games">
+                {games.map(game => (
+                    <div class="game">
+                        <img src={game.thumbnail} alt="Logo Sx" className='image' />
+                        <h1>{game.title}</h1>
+                        <p>{game.publisher}</p>
+                        <p>{game.developer}</p>
+                        <p>{game.platform}</p>
+                        <p>{game.genre}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,7 +8,7 @@ function Home() {
             <img src={SxLogo} alt="Logo Sx" className='image' />
             <h1 className='title'>Bem vindo !</h1>
             <p className='text'>Junte-se a milhões de jogadores e faça novos amigos! Explore um mundo de possibilidades e torne-se um mestre dos games. A diversão está apenas um clique de distância.</p>
-            <button>Jogue agora</button>
+            <a className='button' href="/games">Play now</a>
         </div>
     )
 }
