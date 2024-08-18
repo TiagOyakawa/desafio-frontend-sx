6edbb7a894327ccd8c0b62474ce6ee897ef58d05
Tiago Pirico Oyakawa <tiagoyakawa@hotmail.com>
2024-08-18 11:58:14 -0300

    feat(game-list): ajuste de header e traducao da pagina

--- src/pages/GameList/index.jsx
+++ src/pages/GameList/index.jsx
@@ -1,5 +1,6 @@
 import './style.css'
 import SxLogo from './../../assets/LOGO SX.png'
+import IconBack from './../../assets/VOLTAR-BOTAO.svg'
 import MockGameList from '../../mock/gameList.json'
 
 function GameList() {
@@ -8,20 +9,28 @@ function GameList() {
     return (
         <div className='container-game-list'>
             <header>
-                <img src={SxLogo} alt="Logo Sx" className='header-image' />
-                <ul className='menu'>
-                    <li>
-                        <a href="/">Home</a>
-                    </li>
-                    <li>
-                        <a href="/games">Games</a>
-                    </li>
-                </ul>
+                <a href="/">
+                    <img src={SxLogo} alt="Logo Sx" className='header-image' />
+                </a>
+                <a href="/" className='button button--small' >
+                    <img src={IconBack} alt="link to return to home screen" className='header-icon' />
+                </a>
             </header>
-            <div class="games">
+            <div className='describe'>
+                <h1>
+                    Game List
+                </h1>
+                <p>
+                    Come and discover new exciting games for your lives!
+                </p>
+                <p>
+                    Perfect for streamers looking for fresh, engaging content.
+                </p>
+            </div>
+            <div className="games">
                 {games.map(game => (
-                    <div class="game">
-                        <img src={game.thumbnail} alt="Logo Sx" className='image' />
+                    <div className="game" key={game.id}>
+                        <img src={game.thumbnail} alt={`Game Thumbnail ${game.title}`} className='image' />
                         <h1>{game.title}</h1>
                         <p>{game.publisher}</p>
                         <p>{game.developer}</p>
